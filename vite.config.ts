f2352a48b534b05ca0da3645bc6ca1d90c391130
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2025-01-27 16:56:56 +0700

    feat: fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/memory-app/',
+  plugins: [react()],
 })
